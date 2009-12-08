5f84507e2774639d33b054a3bd740db35db93d0b
Yeison Rodriguez <jaynano@gmail.com>
2009-12-08 00:43:12 -0500

    Initial Commit

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "main.h"
+
+int main (int argc, const char * argv[]) {
+    
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+/*
+ *  untitled.h
+ *  banker
+ *
+ *  Created by Yeison Rodriguez on 12/8/09.
+ *  Copyright 2009 New York University. All rights reserved.
+ *
+ */
+
